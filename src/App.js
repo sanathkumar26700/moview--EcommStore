4df49ab5f161c886877047214098dc58147abca1
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-04-07 04:04:52 +0530

    Add navBar and navBarRoutes components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import NavBarRoutes from "./Routes/navbar-routes";
+
 
 function App() {
     return ( 
     <div className = "App" >
+    <Navbar/>
+    <NavBarRoutes/>
     </div>
     );
 }
