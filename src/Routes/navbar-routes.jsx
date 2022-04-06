3fcd77dcadf7b72215800cea8d23a7dc55d54f23
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-04-07 04:07:04 +0530

    create navbar route component

---
+++ src/Routes/navbar-routes.jsx
@@ -0,0 +1,21 @@
+import {Routes, Route} from "react-router-dom"; 
+import Home from "../Pages/Home/home";
+import ProductList from "../Pages/Product list/product-list";
+import CartList from "../Pages/Cart List/cart-list";
+import WishList from "../Pages/Wish List/wish-list";
+import LoginPage from "../Pages/Login/login-page";
+
+function NavBarRoutes() {
+    return (
+    <Routes>
+        <Route path = "/" element={<Home/>}/>
+        <Route path = "/home" element={<Home/>}/>
+        <Route path = "/product-list" element={<ProductList/>}/>
+        <Route path = "/cart-list" element={<CartList/>}/>
+        <Route path = "/wish-list" element={<WishList/>}/>
+        <Route path = "/login-page" element={<LoginPage/>}/>
+        </Routes>
+    );
+}
+
+export default NavBarRoutes;
