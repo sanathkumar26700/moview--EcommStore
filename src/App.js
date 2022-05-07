bd60d1d71e4ee6cf480a8a5db875b046dfc2dcc4
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-08 00:45:53 +0530

    Add toastify component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import NavBarRoutes from "./Routes/navbar-routes";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 
 function App() {
@@ -8,6 +12,18 @@ function App() {
     <div className = "App" >
     <Navbar/>
     <NavBarRoutes/>
+    <ToastContainer 
+        className="toastify"
+        position="bottom-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        />
     </div>
     );
 }
