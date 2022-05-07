dec7ba2ad5c370ce035b95ff627b4e4d7090628b
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-08 00:48:56 +0530

    Create Mockman component

---
+++ src/Components/Mockman/mock-man.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Mockman from "mockman-js";
+
+function MockAPI() {
+  return (
+    <div style={{fontSize : "1.5rem"}} className="MockAPI">
+      <Mockman />
+    </div>
+  );
+}
+
+export default MockAPI;
