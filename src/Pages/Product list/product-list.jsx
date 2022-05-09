e9802459031a1917f68d370003522ff5ecc074a1
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-09 21:34:49 +0530

    Refactor : filter functions

--- src/Pages/Product list/product-list.jsx
+++ src/Pages/Product list/product-list.jsx
@@ -10,12 +10,7 @@ const ProductList = () => {
 
     const {state:{data, isLoading, sortFrom, excludeOutOfStock, pricing, selectedCategories, rating, searchFor}} = useDataContext()
 
-    const filteredBySelectedCategories = filterBySelectedCategories(data, selectedCategories)
-    const filteredDataBySort = filterDataBySort(filteredBySelectedCategories, sortFrom)
-    const filteredDataByExcludingOutOfStock = filterDataByExcludingOutOfStock(filteredDataBySort, excludeOutOfStock)
-    const filteredDataByPricing = filterDataByPricing(filteredDataByExcludingOutOfStock, pricing)
-    const filteredDataByRating = filterDataByRating(filteredDataByPricing, rating)
-    const finalData = filterBySearch(filteredDataByRating, searchFor)
+    const finalData = filterBySearch(filterDataByRating(filterDataByPricing(filterDataByExcludingOutOfStock(filterDataBySort(filterBySelectedCategories(data, selectedCategories), sortFrom), excludeOutOfStock), pricing), rating), searchFor)
 
     return (
     <section className="product-list__container">
