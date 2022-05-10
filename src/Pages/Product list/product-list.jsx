e5500cfe79e62a8973e9b30fe4f09f01f5fde25c
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-10 23:54:20 +0530

    Refactor filter functions

--- src/Pages/Product list/product-list.jsx
+++ src/Pages/Product list/product-list.jsx
@@ -10,30 +10,25 @@ const ProductList = () => {
 
     const {state:{data, isLoading, sortFrom, excludeOutOfStock, pricing, selectedCategories, rating, searchFor}} = useDataContext()
 
-    const filteredBySelectedCategories = filterBySelectedCategories(data, selectedCategories)
-    const filteredDataBySort = filterDataBySort(filteredBySelectedCategories, sortFrom)
-    const filteredDataByExcludingOutOfStock = filterDataByExcludingOutOfStock(filteredDataBySort, excludeOutOfStock)
-    const filteredDataByPricing = filterDataByPricing(filteredDataByExcludingOutOfStock, pricing)
-    const filteredDataByRating = filterDataByRating(filteredDataByPricing, rating)
-    const finalData = filterBySearch(filteredDataByRating, searchFor)
+    const finalData = filterBySearch(filterDataByRating(filterDataByPricing(filterDataByExcludingOutOfStock(filterDataBySort(filterBySelectedCategories(data, selectedCategories), sortFrom), excludeOutOfStock), pricing), rating), searchFor)
 
     return (
-    <section className="product-list__container">
-            <Filter/>
-        {isLoading ? 
-        <img className="loader--image" src="https://cutewallpaper.org/21/loading-gif-transparent-background/HopeWell.gif" alt="loader"/> 
-        :
-        <>
-            {finalData.length ?
-                <section className="product-list__section ">
-                    {finalData.map((product) => <SingleProduct product = {product} key={product._id}/>)}
-                </section>
-            : 
-            <h1 className = "Product-list__empty--text">Products Unavailable 😣</h1>
+        <section className="product-list__container">
+                <Filter/>
+            {isLoading ? 
+            <img className="loader--image" src="https://cutewallpaper.org/21/loading-gif-transparent-background/HopeWell.gif" alt="loader"/> 
+            :
+            <>
+                {finalData.length ?
+                    <section className="product-list__section ">
+                        {finalData.map((product) => <SingleProduct product = {product} key={product._id}/>)}
+                    </section>
+                : 
+                <h1 className = "Product-list__empty--text">Products Unavailable 😣</h1>
+                }
+            </>
             }
-        </>
-        }
-    </section>
+        </section>
     );
 };
 
