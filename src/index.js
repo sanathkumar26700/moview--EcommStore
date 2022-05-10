9849eb531fbe7bd790d7960b200c2ceeb3f324e4
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-11 00:16:51 +0530

    Add data context provider

--- src/index.js
+++ src/index.js
@@ -6,18 +6,21 @@ import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import {DataContextProvider} from "./Context/dataContext"
 import {AuthProvider} from "./Context/authContext"
+import {UserDataProvider} from "./Context/userDataContext"
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-  <AuthProvider>
+    <AuthProvider>
       <DataContextProvider>
-        <BrowserRouter>
-         <App />
-        </BrowserRouter>
-      </DataContextProvider>
+        <UserDataProvider>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </UserDataProvider>
+        </DataContextProvider>
       </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
