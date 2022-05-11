facd85647d615eb42c32793ea68391e1b91f80a9
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-11 16:01:17 +0530

    fix responsiveness of filter in product listing page

--- src/Pages/Product list/product-list.jsx
+++ src/Pages/Product list/product-list.jsx
@@ -1,4 +1,4 @@
-import {React} from 'react';
+import {useState} from 'react';
 import SingleProduct from '../../Components/Single Product/single-product'
 import Filter from '../../Components/Filter/filter';
 import {useDataContext} from '../../Context/dataContext'
@@ -8,13 +8,21 @@ import './product-list.css';
 
 const ProductList = () => {
 
+    const [display, setDisplay] = useState('')
     const {state:{data, isLoading, sortFrom, excludeOutOfStock, pricing, selectedCategories, rating, searchFor}} = useDataContext()
 
     const finalData = filterBySearch(filterDataByRating(filterDataByPricing(filterDataByExcludingOutOfStock(filterDataBySort(filterBySelectedCategories(data, selectedCategories), sortFrom), excludeOutOfStock), pricing), rating), searchFor)
 
     return (
+        <>
+        <div class="filter__action--container">
+        {((display === 'none') || (display === '')) &&
+                <button onClick={() => setDisplay('block')} class="btn btn__underlined"><i class="fas fa-filter" aria-hidden="true"></i></button>}
+        {(display === 'block') && 
+                <button onClick={() => setDisplay('none')} class="btn btn__underlined"><i class="fas fa-times" aria-hidden="true"></i></button>}
+        </div>
         <section className="product-list__container">
-                <Filter/>
+                <Filter display={display}/>
             {isLoading ? 
             <img className="loader--image" src="https://cutewallpaper.org/21/loading-gif-transparent-background/HopeWell.gif" alt="loader"/> 
             :
@@ -29,6 +37,7 @@ const ProductList = () => {
             </>
             }
         </section>
+        </>
     );
 };
 
