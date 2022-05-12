5d4a3878db3c15538131d1d67440c24303309a89
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-12 20:26:31 +0530

    Create cartlist page

--- src/Pages/Cart List/cart-list.jsx
+++ src/Pages/Cart List/cart-list.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import SingleProduct from '../../Components/Single Product/single-product'
+import { useUserData } from '../../Context/userDataContext';
+import BillBox from './Components/billBox'
+import './cart-list.css'
+
 
 const CartList = () => {
+
+    const {userData:{cartListData}, isLoading} = useUserData()
+
     return (
-        <div>
-            Cart list
-        </div>
+        <>
+            {isLoading ? 
+                <img className="loader--image" src="https://cutewallpaper.org/21/loading-gif-transparent-background/HopeWell.gif" alt="loader"/> 
+            :
+            <>
+                <div className="page--heading">
+                    <span>
+                        My cart({cartListData.length})
+                    </span>
+                </div>                          
+               { cartListData.length ?  
+                    <section className="cart-list__section ">
+                        <section className="card-list__container">
+                            {cartListData.map((product) => <SingleProduct cardDirection={'horizontal'} product = {product} key={product._id}/>)}
+                        </section>   
+                            <BillBox/>
+                    </section>
+                :
+                    <h1 className = "Product-list__empty--text">Your cart is empty 🙄</h1>}
+            </>
+            }
+        </>
     );
 };
-
-export default CartList;
+export default CartList
