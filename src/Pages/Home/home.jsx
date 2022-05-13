d6ad45a38e0658d28f5a0f8795d49a09a4924e1b
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-14 02:18:40 +0530

    fix : class syntax

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -19,46 +19,46 @@ const Home = () => {
 
     return (
         <>
-            <section class="hero__container">
-                <div class="hero__text--container">
-                    <h1 class="hero--text">Bring back the lost glory of CDs !</h1>
-                    <Link  to='/product-list' class="btn btn__primary btn__primary--animated">shop now</Link>
+            <section className="hero__container">
+                <div className="hero__text--container">
+                    <h1 className="hero--text">Bring back the lost glory of CDs !</h1>
+                    <Link  to='/product-list' className="btn btn__primary btn__primary--animated">shop now</Link>
                 </div>
-                <img src="https://moview-store.netlify.app/images/Compact_disc.svg.png" alt="compact-disk img" class="hero--img "/>
+                <img src="https://moview-store.netlify.app/images/Compact_disc.svg.png" alt="compact-disk img" className="hero--img "/>
             </section>
-            <section class="category__section">
-                <div class="category__container">
-                    <h1 class="category__title">Popular Categories</h1>
+            <section className="category__section">
+                <div className="category__container">
+                    <h1 className="category__title">Popular Categories</h1>
                     <Link to='/product-list'>
                         <figure 
                             onClick = {() =>  dispatch({type : "SORT_BY_CATEGORIES" , payload : categoryHandler("Blu-ray")})}
-                            class="category--img">
+                            className="category--img">
                             <img src="https://github.com/sanathkumar26700/moview__EcomStore--ui/blob/development/images/product-images/Landing-page/blu-ray--logo.png?raw=true" alt="bluray--logo.jpg" />
                         </figure>
                     </Link>
                     <Link  to='/product-list'>
                         <figure 
                             onClick = {() =>  dispatch({type : "SORT_BY_CATEGORIES" , payload : categoryHandler("DVD")})}
-                            class="category--img">
+                            className="category--img">
                             <img src="https://github.com/sanathkumar26700/moview__EcomStore--ui/blob/development/images/product-images/Landing-page/DVD--logo.png?raw=true" alt="DVD--logo.png"/>
                         </figure>
                     </Link>
                     <Link to='/product-list'>
                         <figure 
                             onClick = {() =>  dispatch({type : "SORT_BY_CATEGORIES" , payload : categoryHandler("VCD")})}
-                            class="category--img">
+                            className="category--img">
                             <img src="https://github.com/sanathkumar26700/moview__EcomStore--ui/blob/development/images/product-images/Landing-page/vcd--logo.png?raw=true" alt="VCD--logo.png "/>
                         </figure>
                     </Link>
                 </div>
             </section>
-            <section class="trending-list__container">
-            <h1 class="product-list__title" id="product-list">Trending movies</h1>
-                <section class="product-list__section">
+            <h1 className="product-list__title" id="product-list">Trending movies</h1>
+            <section className="trending-list__container">
+                <section className="product-list__section">
                     {trendingMovies.map((product) => <SingleProduct cardDirection={'vertical'} product = {product} key={product._id}/>)}
-                    <div class="vertical flex--center">
-                        <Link  to='/product-list' class="btn btn__underlined btn__underlined--animated">
-                            see more<i class="fas fa-chevron-right btn__icon-right"></i>
+                    <div className="vertical flex--center">
+                        <Link  to='/product-list' className="btn btn__underlined btn__underlined--animated">
+                            see more<i className="fas fa-chevron-right btn__icon-right"></i>
                         </Link>
                     </div>
                 </section>
